67ccae545bdaa4b736a17acccb76cc0943c63455
Umair Ashraf <umairjee360@gmail.com>
2026-04-23 22:11:21 +0500

    only backend project

--- vision-code-frontend/src/hooks/useCourseDetail.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCourseDetail } from "../services/courseDetail";
-export function useCourseDetail(courseId) {
-  return useQuery({
-    queryKey: ["course", courseId],
-    queryFn: () => getCourseDetail(courseId),
-    enabled: !!courseId,
-  });
-}
